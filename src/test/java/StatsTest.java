664fd4d161e217d0d2fbe023a279769a4bf581e1
Valeryi Dnkovtsev <valorapaulinio@gmail.com>
2022-12-19 12:45:50 +0300

    change pom

--- src/test/java/StatsTest.java
+++ src/test/java/StatsTest.java
@@ -1,9 +1,6 @@
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.StatsService;
-
-import java.util.SplittableRandom;
 
 public class StatsTest {
     @Test
